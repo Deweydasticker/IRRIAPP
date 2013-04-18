6988afc50566d67ec077927b66786af215514c03
unknown <Mark@.(none)>
2013-04-18 21:33:44 +0200

    styling

--- js/ajax.js
+++ js/ajax.js
@@ -1,12 +1,18 @@
 /**
  * @author Dewey
  */
-   $(function() { // execute on document ready
-       $('#refreshButton').click( function() { // add the click event handler to button
-       	console.log("test");
-          $.getJSON( 'http://195.211.73.185/~irriapp/index.php/Words', function(result) {
-                // do something with the result
-                console.log(result);
-          });
-       });
-   });
+$(function() { // execute on document ready
+	$('#refreshButton').click( function() { // add the click event handler to button
+		updateWord();
+	});
+       
+	function updateWord(){
+		$.getJSON( 'http://195.211.73.185/~irriapp/index.php/Words', function(result) {
+		// do something with the result
+		$('#wordText').html(result.word);
+		$('#definitionText').html(result.description);
+		$('#exampleText').html(result.example);
+		});
+	}
+	updateWord();      
+});
